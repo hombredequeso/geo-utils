349d1e8b2dbb38d3a68f821677a99c5e1900916d
mcheeseman <mcheeseman@users.noreply.github.com>
2017-05-28 01:24:35 +1000

    Random coord and variation done
    Ability to randomly pick from weighted list.
    Random variation up to a certain distance from a coord.

--- test/random-geo-coord.spec.js
+++ test/random-geo-coord.spec.js
@@ -7,6 +7,19 @@ let expect = chai.expect;
 
 describe('random-geo-coord', function() {
     describe('get', function() {
+
+        let createItem = i => {
+            return {
+                    name: `place#${i}`,
+                    population: 1,
+                    coord: {
+                        lat: i * 100,
+                        lon: (i*100) + 1 
+                    }
+            };
+
+        }
+
         it('throws exception when nothing in geoData list', function() {
             let f = () => randomGeoCoord.get([]);
             expect(f).to.throw("No data in geoData array")
@@ -26,15 +39,44 @@ describe('random-geo-coord', function() {
 
             let result = randomGeoCoord.get(geoData);
 
-            let expectedResult = 
-                {
-                    place: geoData[0],
-                    coord: {
-                        lat: 1,
-                        lon: 2
-                    }
-                };
-            expect(result).to.eql(expectedResult);
+            expect(result).to.eql(geoData[0]
+            );
+        });
+
+        it('returns 2 equally weighted items about 50% of the time', function() {
+            let geoData = Array(2).fill(0).map((_, i) => createItem(i));
+            let iterations = 1000;
+
+            let result = Array(iterations).fill(0).map(_ => randomGeoCoord.get(geoData));
+
+            let groupedResults = _.groupBy(result, x => x.name);
+
+            expect(_.keys(groupedResults).length).to.equal(2);
+        })
+    });
+
+    describe('randomizedCoord', function() {
+        it('should return a random coordinate', function() {
+            let result = randomGeoCoord.randomizedCoord(Math.random, {lat: 1, lon: 2});
+            expect(result.lat).to.be.a('number');
+            expect(result.lon).to.be.a('number');
+        });
+        it('with no variance should return the same number', function() {
+            let randomizer = () => 0;
+            let coord = {lat: 1, lon: 2};
+            let result = randomGeoCoord.randomizedCoord(randomizer, coord, 100);
+            expect(result).to.eql(coord);
+        });
+        it('with full variance should return the same number', function() {
+            let distanceBetweenLats = 111.32;
+            let l = [0.25, 1];  
+            let randomizer = function() {
+                return l.pop();
+            }
+            let coord = {lat: 1, lon: 2};
+            let result = randomGeoCoord.randomizedCoord(
+                randomizer, coord, distanceBetweenLats);
+            expect(result).to.eql({lat: 2, lon:2})
         });
     });
 });
---
+++ test/weighted-random.spec.js
@@ -0,0 +1,102 @@
+var weightedRandom = require('../src/weighted-random');
+var chai = require('chai');
+var _ = require('lodash');
+
+var expect = chai.expect;
+
+let getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let getWeightedRandom = _.curry(weightedRandom.get)(getRandomIntInclusive)("weight");
+
+describe('get', function() {
+    it('returns sole item from one element list', function() {
+        let soleItem = {value: 1, weight: 2};
+        let result = getWeightedRandom([soleItem])
+        expect(result).to.equal(soleItem);
+    });
+
+    it('throws exception if nothing in weighted array', function() {
+        let f = function() {getWeightedRandom([]);}
+        expect(f).to.throw('weightedElements cannot be empty')
+    });
+
+    it('throws exception if totalWeights are zero', function() {
+        let items = (new Array(10))
+            .fill(0)
+            .map((v,i) => {return {value: i, weight: 0};});
+        let f = function() {getWeightedRandom(items);}
+        expect(f).to.throw('total weights must be greater than 0')
+    });
+
+    it('returns roughly even distribution of equally weighted elements', function() {
+        let itemCount = 2;
+        let items = (new Array(itemCount))
+            .fill(0)
+            .map((v,i) => {return {value: i, weight: 1};});
+
+        let totals = new Array(items.length).fill(0);
+
+        let iterations = 1000;
+        for(let i=0;i<iterations;i++) {
+            let item = getWeightedRandom(items);
+            totals[item.value]++;
+        }
+
+        let expectedCount = iterations / items.length;
+        let allowedVariation = 100;
+
+        totals.forEach(t => 
+            expect(t).to.be.closeTo(expectedCount, allowedVariation))
+    });
+});
+
+describe('getScaledRandom', function() {
+    it('returns a result between min (inclusive) and max (exclusive)', function() {
+        let min = 0;
+        let max = 10;
+        let zeroWeighting = 2;
+
+        let result = weightedRandom.getScaledRandom(Math.random, min, max, 1);
+
+        expect(result).to.be.at.least(min);
+        expect(result).to.be.below(max);
+    });
+
+    it('returns linearly distributed results for factor of 1', function() {
+        let min = 0;
+        let max = 10;
+        let totals = new Array(max - min).fill(0);
+
+        let iterations = 10000;
+        for(let i=0;i<iterations;i++) {
+            let result = weightedRandom.getScaledRandom(Math.random, min, max, 1);
+            totals[Math.floor(result)]++;
+        }
+
+        let expectedCount = iterations / (max - min);
+        let allowedVariation = expectedCount * 0.1;
+        totals.forEach(t => 
+            expect(t).to.be.closeTo(expectedCount, allowedVariation))
+    });
+
+    it('returns non-linearly distributed results for factor of 1.5', function() {
+        let min = 0;
+        let max = 10;
+        let totals = new Array(max - min).fill(0);
+
+        let iterations = 50000;
+        for(let i=0;i<iterations;i++) {
+            let result = weightedRandom.getScaledRandom(Math.random, min, max, 1.5);
+            totals[Math.floor(result)]++;
+        }
+
+        for(let z=1; z < totals.length; z++) {
+            expect(totals[z]).to.be.below(totals[z-1]);
+        }
+    });
+});
+
