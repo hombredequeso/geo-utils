e058cc7211ab8854c34a690f6b77ee361b418408
mcheeseman <mcheeseman@users.noreply.github.com>
2017-05-30 15:10:46 +1000

    Simplistic loading of files for module
    Add index.js file that explicitly loads each file in the /src.

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.bingGeo = require('./src/bing-geo');
+exports.randomGeoCoord = require('./src/random-geo-coord');
+exports.urlUtil = require('./src/url-util');
+exports.weightedRandom = require('./src/weighted-random');
